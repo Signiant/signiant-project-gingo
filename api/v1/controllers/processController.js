2dc2c9956cc790d89e75dec4d496d66013730a5f
Scott Reynolds <scott@scottreynolds.net>
2021-02-24 13:37:36 -0800

    removed querystring

--- api/v1/controllers/processController.js
+++ api/v1/controllers/processController.js
@@ -6,7 +6,7 @@ module.exports.processController = (req, res) => {
     // a slight delay is required while the package data is stored from the metadata submit. Package data is also available in portalPackage above but demonstrates subsequent retrievals via the API
     setTimeout(()=>{}, 1000)
     console.log('req.body:', req.body)
-    const form = querystring.parse(req.body);
+    const form = req.body;
     console.log('form:',form)
     console.log('form.redirectUrl:', form.redirectUrl)
     const signedUrl = generateSignedUrl(form.redirectUrl, req.body, registrationKey);

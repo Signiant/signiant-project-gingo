0715c6b9c723626f84939b9c11e8bbe901c9274a
Scott Reynolds <scott@scottreynolds.net>
2021-02-24 14:46:29 -0800

    final testing

--- api/v1/controllers/processController.js
+++ api/v1/controllers/processController.js
@@ -5,13 +5,8 @@ const registrationKey = process.env.registrationKey;
 module.exports.processController = (req, res) => {
     // a slight delay is required while the package data is stored from the metadata submit. Package data is also available in portalPackage above but demonstrates subsequent retrievals via the API
     setTimeout(()=>{}, 1000)
-    // console.log('req.body:', req.body)
     const form = querystring.parse(req.body);
-    // console.log('form:',(typeof form))
-    // console.log('form.redirectUrl:', form.redirectUrl)
     const signedUrl = generateSignedUrl(form.redirectUrl, req.body, registrationKey);
-    // console.log('signedUrl:', signedUrl)
     res.set('Location', signedUrl);
-    // console.log('res.set')
     return res.status(307).end();
 }
--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const cors = require('cors');
 const port = process.env.PORT || 3000;
 const routes = require('./api/v1/controllers/index');
 
-// app.use(cors())
-// app.use(express.urlencoded({extended: true}));
-// app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/',
     (req, res, next) => {

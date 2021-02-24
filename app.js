2ededb761e75df56caadc78273d0e7d544ae4ffd
Scott Reynolds <scott@scottreynolds.net>
2021-02-24 14:39:26 -0800

    removed unused app methods

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ const cors = require('cors');
 const port = process.env.PORT || 3000;
 const routes = require('./api/v1/controllers/index');
 
-app.use(cors())
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+// app.use(cors())
+// app.use(express.urlencoded({extended: true}));
+// app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/',
     (req, res, next) => {
         console.log(`${req.method} ${req.originalUrl}`)
         console.log(`req: ${req}`)
         next()
     }, routes);
-app.use(express.static(path.join(__dirname, 'public')));
 app.listen(port, () => {
     console.log('App listening on ' + port + ' @ ' + (new Date()).toLocaleString());
 });

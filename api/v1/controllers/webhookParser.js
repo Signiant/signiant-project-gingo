5a43ceb151214427933c84d1d7cebb06e857b50f
Scott Reynolds <scott@scottreynolds.net>
2021-02-24 17:15:20 -0800

    finding payload

--- api/v1/controllers/webhookParser.js
+++ api/v1/controllers/webhookParser.js
@@ -1,7 +1,8 @@
 const { sendmail } = require('../../../components/sendForm')
 
 module.exports.webhookParser = async (req, res) => {
-    let {payload} = req.body.data
+    console.log('req', req)
+    let {payload} = req
     console.log('payload: ', payload)
     await sendmail('scott', 'jim', 'test', 'test body')
     return res.status(200).json(payload)

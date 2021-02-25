e17ba6a6feeba4617c4e4ddbc2e6efc2a6eb6b31
Scott Reynolds <scott@scottreynolds.net>
2021-02-24 17:30:55 -0800

    bodyparser

--- api/v1/controllers/webhookParser.js
+++ api/v1/controllers/webhookParser.js
@@ -1,8 +1,8 @@
 const { sendmail } = require('../../../components/sendForm')
 
 module.exports.webhookParser = async (req, res) => {
-    console.log('req', req)
-    let {payload} = req
+    console.log('req', req.body)
+    let {payload} = req.body
     console.log('payload: ', payload)
     await sendmail('scott', 'jim', 'test', 'test body')
     return res.status(200).json(payload)

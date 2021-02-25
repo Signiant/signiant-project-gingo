6f65d2d72c6a12eb9f5c2a5462354a6a3a8213ae
Scott Reynolds <scott@scottreynolds.net>
2021-02-25 12:28:17 -0800

    adding sendmail testing

--- api/v1/controllers/webhookParser.js
+++ api/v1/controllers/webhookParser.js
@@ -1,13 +1,13 @@
 const { sendmail } = require('../../../components/sendMail')
-const ms = require('@concentricity/media_shuttle_components')
+const { ms } = require('@concentricity/media_shuttle_components')
 
 
 module.exports.webhookParser = async (req, res) => {
     let {payload} = req.body
+    console.log('payload: ', payload)
 
     const emails = await ms.getPortalsUsers(payload.portalDetails.id)
 
-    console.log('payload: ', payload)
     await sendmail(emails, 'From Me', 'Test subject', 'Test message body')
     return res.status(200).json(payload)
 }
--- components/sendMail.js
+++ components/sendMail.js
@@ -1,5 +1,8 @@
 
 module.exports.sendMail = (to, from, subject, body) => {
+    to.map( item => {
+        console.log('to item:', item)
+    })
     console.log('sendmail details:', to, from, subject, body)
     return 
 }

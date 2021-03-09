7e057c5206cd7fa2ec0a2c803b30d0f4a906a687
Scott Reynolds <scott@scottreynolds.net>
2021-03-09 11:01:51 -0800

    updating timeout

--- api/v1/controllers/showController.js
+++ api/v1/controllers/showController.js
@@ -2,7 +2,7 @@ const rp = require('request-promise');
 const axios = require('axios');
 const ejs = require('ejs');
 const registrationKey = process.env.registrationKey;
-const {generateSignedUrl} = require('../../../components/generateSignedUrl')
+const { generateSignedUrl } = require('../../../components/generateSignedUrl')
 const formUrl = process.env.formUrl;
 
 module.exports.showController = async (req, res) => {
@@ -13,51 +13,52 @@ module.exports.showController = async (req, res) => {
     3. The package endpoint url is the same as the redirectUrl without the /metadata suffix.
     */
 
-    const portalPackageUrl = req.body.redirectUrl.replace(/\/metadata$/, '');
-
-    // Generate a signed url for the above using the portal registration key.
-    const signedPortalPackageUrl = generateSignedUrl(portalPackageUrl, '', registrationKey);
-
-    // Restore this refactoring to include timeout after more testing
-
-    // Fetch the package details from Media Shuttle
-    let portalPackageJson
-    try {
-        console.log('here2')
-        let params1 = {
-            method: 'GET',
-            params: { BaseURL: signedPortalPackageUrl }
-        }
-
-        setTimeout( async () => {
+   setTimeout(async () => {
+        const portalPackageUrl = req.body.redirectUrl.replace(/\/metadata$/, '');
+     
+        // Generate a signed url for the above using the portal registration key.
+        const signedPortalPackageUrl = generateSignedUrl(portalPackageUrl, '', registrationKey);
+     
+        // Restore this refactoring to include timeout after more testing
+     
+        // Fetch the package details from Media Shuttle
+        let portalPackageJson
+        try {
+            console.log('here2')
+            let params1 = {
+                method: 'GET',
+                params: { BaseURL: signedPortalPackageUrl }
+            }
             const portalPackage = await axios(params1)
-        }, 500)
-        console.log('portalPackage', portalPackage)
-        const portalPackageJson = JSON.parse(portalPackage)
-        console.log('portalPackageJson', portalPackageJson)
-    } catch (error) {
-        return res.status(500).send(error.message).end();
-    }
-
-    // Return the form with the template values
-    console.log('portalPackageJson', portalPackageJson)
-    try {
-        let params2 = {
-            method: 'GET',
-            params: { baseURL: formUrl }
+            console.log('portalPackage', portalPackage)
+            const portalPackageJson = JSON.parse(portalPackage)
+            console.log('portalPackageJson', portalPackageJson)
+        } catch (error) {
+            return res.status(500).send(error.message).end();
         }
+     
+        // Return the form with the template values
+        console.log('portalPackageJson', portalPackageJson)
+        try {
+            let params2 = {
+                method: 'GET',
+                params: { baseURL: formUrl }
+            }
+     
+            const form = await axios(params2)
+            console.log('form', form)
+     
+            res.send(ejs.render(form, {
+                redirectUrl: req.body.redirectUrl,
+                senderEmail: portalPackageJson.packageDetails.sender
+            }));
+        } catch (error) {
+            return res.status(500).send(error.message).end();
+        }
+
+    }, 500)
     
-        const form = await axios(params2)
-        console.log('form', form)
-    
-        res.send(ejs.render(form, {
-            redirectUrl: req.body.redirectUrl,
-            senderEmail: portalPackageJson.packageDetails.sender
-        }));
-    } catch (error) {
-        return res.status(500).send(error.message).end();
-    }
-    
+
 
     // setTimeout( () => {
     //     rp.get(signedPortalPackageUrl)

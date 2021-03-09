020843e4d383b5efe1dba6e88411e50c9585e0a3
Scott Reynolds <scott@scottreynolds.net>
2021-03-09 11:27:57 -0800

    restored rp methods

--- api/v1/controllers/showController.js
+++ api/v1/controllers/showController.js
@@ -13,71 +13,71 @@ module.exports.showController = async (req, res) => {
     3. The package endpoint url is the same as the redirectUrl without the /metadata suffix.
     */
 
-        const portalPackageUrl = req.body.redirectUrl.replace(/\/metadata$/, '');
-     
-        // Generate a signed url for the above using the portal registration key.
-        const signedPortalPackageUrl = generateSignedUrl(portalPackageUrl, '', registrationKey);
+    const portalPackageUrl = req.body.redirectUrl.replace(/\/metadata$/, '');
 
-        console.log('signedPortalPackageUrl', signedPortalPackageUrl)
+    // Generate a signed url for the above using the portal registration key.
+    const signedPortalPackageUrl = generateSignedUrl(portalPackageUrl, '', registrationKey);
 
-        // Restore this refactoring to include timeout after more testing
-     
-        // Fetch the package details from Media Shuttle
-        let portalPackageJson
-        // try {
-            console.log('here1')
-            let params1 = {
-                method: 'GET',
-                params: { url: signedPortalPackageUrl }
-            }
-            console.log('here2')
-            setTimeout( async () => {
-                console.log('here3')
-                const portalPackage = await axios(params1)
-                console.log('here4')
-                console.log('portalPackage', portalPackage)
-                const portalPackageJson = JSON.parse(portalPackage)
-                console.log('portalPackageJson', portalPackageJson)
-            }, 500)
-        // } catch (error) {
-        //     return res.status(500).send(error.message).end();
-        // }
-     
-        // Return the form with the template values
-        console.log('portalPackageJson', portalPackageJson)
-        // try {
-            let params2 = {
-                method: 'GET',
-                params: { url: formUrl }
-            }
-     
-            const form = await axios(params2)
-            console.log('form', form)
-     
-            res.send(ejs.render(form, {
-                redirectUrl: req.body.redirectUrl,
-                senderEmail: portalPackageJson.packageDetails.sender
-            }));
-        // } catch (error) {
-        //     return res.status(500).send(error.message).end();
-        // }
-    
+    // console.log('signedPortalPackageUrl', signedPortalPackageUrl)
+
+    // // Restore this refactoring to include timeout after more testing
+
+    // // Fetch the package details from Media Shuttle
+    // let portalPackageJson
+    // // try {
+    //     console.log('here1')
+    //     let params1 = {
+    //         method: 'GET',
+    //         params: { url: signedPortalPackageUrl }
+    //     }
+    //     console.log('here2')
+    //     setTimeout( async () => {
+    //         console.log('here3')
+    //         const portalPackage = await axios(params1)
+    //         console.log('here4')
+    //         console.log('portalPackage', portalPackage)
+    //         const portalPackageJson = JSON.parse(portalPackage)
+    //         console.log('portalPackageJson', portalPackageJson)
+    //     }, 500)
+    // // } catch (error) {
+    // //     return res.status(500).send(error.message).end();
+    // // }
+
+    // // Return the form with the template values
+    // console.log('portalPackageJson', portalPackageJson)
+    // // try {
+    //     let params2 = {
+    //         method: 'GET',
+    //         params: { url: formUrl }
+    //     }
+
+    //     const form = await axios(params2)
+    //     console.log('form', form)
+
+    //     res.send(ejs.render(form, {
+    //         redirectUrl: req.body.redirectUrl,
+    //         senderEmail: portalPackageJson.packageDetails.sender
+    //     }));
+    // // } catch (error) {
+    // //     return res.status(500).send(error.message).end();
+    // // }
 
 
-    // setTimeout( () => {
-    //     rp.get(signedPortalPackageUrl)
-    //         .then(portalPackage => {
-    //             let portalPackageJson = JSON.parse(portalPackage);
-    //             return rp.get(formUrl)
-    //                 .then(form => {
-    //                     res.send(ejs.render(form, {
-    //                         redirectUrl: req.body.redirectUrl,
-    //                         senderEmail: portalPackageJson.packageDetails.sender
-    //                     }));
-    //                 });
-    //         })
-    //         .catch(err => {
-    //             return res.status(500).send(err.message).end();
-    //         });
-    // }, 500)
+
+    setTimeout(() => {
+        rp.get(signedPortalPackageUrl)
+            .then(portalPackage => {
+                let portalPackageJson = JSON.parse(portalPackage);
+                return rp.get(formUrl)
+                    .then(form => {
+                        res.send(ejs.render(form, {
+                            redirectUrl: req.body.redirectUrl,
+                            senderEmail: portalPackageJson.packageDetails.sender
+                        }));
+                    });
+            })
+            .catch(err => {
+                return res.status(500).send(err.message).end();
+            });
+    }, 500)
 }

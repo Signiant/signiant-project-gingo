24429fb06d11e682037183a4233d39a92474e6c1
Scott Reynolds <scott@scottreynolds.net>
2021-03-08 20:43:40 -0800

    debugging show controller

--- api/v1/controllers/showController.js
+++ api/v1/controllers/showController.js
@@ -18,48 +18,6 @@ module.exports.showController = async (req, res) => {
 
     // Generate a signed url for the above using the portal registration key.
     const signedPortalPackageUrl = await generateSignedUrl(portalPackageUrl, '', registrationKey);
-    console.log('here1')
-    console.log('signedPortalPackageUrl', signedPortalPackageUrl)
-    console.log(typeof signedPortalPackageUrl)
-
-    // Fetch the package details from Media Shuttle
-    // let portalPackageJson
-    // // try {
-    //     console.log('here2')
-    //     let params1 = {
-    //         method: 'GET',
-    //         params: { BaseURL: signedPortalPackageUrl }
-    //     }
-
-    
-    //     const portalPackage = await axios(params1)
-    //     console.log('portalPackage', portalPackage)
-    //     const portalPackageJson = JSON.parse(portalPackage)
-    //     console.log('portalPackageJson', portalPackageJson)
-    // // } catch (error) {
-    // //     return res.status(500).send(error.message).end();
-    // // }
-
-    // //  Return the form with the template values
-    // // console.log('portalPackageJson', portalPackageJson)
-    // // try {
-    //     let params2 = {
-    //         method: 'GET',
-    //         params: { baseURL: formUrl }
-    //     }
-    
-    //     const form = await axios(params2)
-    //     console.log('form', form)
-    
-    //     res.send(ejs.render(form, {
-    //         redirectUrl: req.body.redirectUrl,
-    //         senderEmail: portalPackageJson.packageDetails.sender
-    //     }));
-    // // } catch (error) {
-    // //     return res.status(500).send(error.message).end();
-    // // }
-    
-
 
     rp.get(signedPortalPackageUrl)
         .then(portalPackage => {
--- components/sendForm.js
+++ components/sendForm.js
@@ -1,6 +1,5 @@
 const path = require('path');
 
 module.exports.sendForm = (req, res) => {
-    console.log('SendForm req.body', req.body)
     return res.sendFile(path.join(__dirname, "../public/form.html"));
 }

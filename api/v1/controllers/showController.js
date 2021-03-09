c0d168f5630e47808ced6bf5f008fc688a0fb884
Scott Reynolds <scott@scottreynolds.net>
2021-03-08 21:00:15 -0800

    restored try catch code for future application

--- api/v1/controllers/showController.js
+++ api/v1/controllers/showController.js
@@ -19,7 +19,47 @@ module.exports.showController = async (req, res) => {
     // Generate a signed url for the above using the portal registration key.
     const signedPortalPackageUrl = await generateSignedUrl(portalPackageUrl, '', registrationKey);
 
-    setTimeout(()=>{
+    // Restore this iteration to include timeout after more testing
+
+    // Fetch the package details from Media Shuttle
+    // let portalPackageJson
+    // // try {
+    //     console.log('here2')
+    //     let params1 = {
+    //         method: 'GET',
+    //         params: { BaseURL: signedPortalPackageUrl }
+    //     }
+
+    
+    //     const portalPackage = await axios(params1)
+    //     console.log('portalPackage', portalPackage)
+    //     const portalPackageJson = JSON.parse(portalPackage)
+    //     console.log('portalPackageJson', portalPackageJson)
+    // // } catch (error) {
+    // //     return res.status(500).send(error.message).end();
+    // // }
+
+    // //  Return the form with the template values
+    // // console.log('portalPackageJson', portalPackageJson)
+    // // try {
+    //     let params2 = {
+    //         method: 'GET',
+    //         params: { baseURL: formUrl }
+    //     }
+    
+    //     const form = await axios(params2)
+    //     console.log('form', form)
+    
+    //     res.send(ejs.render(form, {
+    //         redirectUrl: req.body.redirectUrl,
+    //         senderEmail: portalPackageJson.packageDetails.sender
+    //     }));
+    // // } catch (error) {
+    // //     return res.status(500).send(error.message).end();
+    // // }
+    
+
+    setTimeout( () => {
         rp.get(signedPortalPackageUrl)
             .then(portalPackage => {
                 let portalPackageJson = JSON.parse(portalPackage);
@@ -34,6 +74,5 @@ module.exports.showController = async (req, res) => {
             .catch(err => {
                 return res.status(500).send(err.message).end();
             });
-
-    },500)
+    }, 500)
 }

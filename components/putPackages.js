d977ed038aed68895afcd9ec37b8bb9cdca816e5
Scott Reynolds <scott@scottreynolds.net>
2021-04-23 11:55:15 -0700

    debugging fileset

--- components/putPackages.js
+++ components/putPackages.js
@@ -6,10 +6,27 @@ ApiKey.apiKey = process.env.MS_API_KEY
 
 module.exports = (portalId, packageId, files) => {
     console.log('putPackages', portalId, packageId, files)
+    files = [
+        {
+          "path": "scott@scottreynolds.net/YQhkXFPeNuUgd8Ei6EtLOE/blurry-lights.jpg",
+          "isDirectory": false,
+          "size": 902538
+        },
+        {
+          "path": "scott@scottreynolds.net/YQhkXFPeNuUgd8Ei6EtLOE/barcode-font.jpg",
+          "isDirectory": false,
+          "size": 2229417
+        },
+        {
+          "path": "scott@scottreynolds.net/YQhkXFPeNuUgd8Ei6EtLOE/backdrops",
+          "isDirectory": true,
+          "size": 5227442
+        }
+      ]
     return new Promise((resolve, reject) => {
-        console.log(typeof files)
+        console.log('typeof files', typeof files)
         var apiInstance = new MediaShuttleApi.SystemToPersonApi();
-        var fileset = new MediaShuttleApi.FileSetRequest({files: files});
+        var fileset = new MediaShuttleApi.FileSetRequest(files);
         
         console.log('fileset', fileset)
     

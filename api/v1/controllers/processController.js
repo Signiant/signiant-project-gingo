88e4570d0c4aa93d93efe4f65e0227a036c96788
Scott Reynolds <scott@scottreynolds.net>
2021-04-29 11:28:04 -0700

    restored form querstrying.parse

--- api/v1/controllers/processController.js
+++ api/v1/controllers/processController.js
@@ -4,10 +4,12 @@ const { generateSignedUrl } = require('../../../components/generateSignedUrl')
 
 module.exports.processController = (req, res) => {
 
+    const form = querystring.parse(req.body);
+
     console.log('req', req)
     //console.log('JSON req.body', json)
     // lookup portal mapping to determine portal app settings
-    const portalPackageUrl = unescape(body.redirectUrl.replace(/\/metadata$/, ''));
+    const portalPackageUrl = unescape(form.redirectUrl.replace(/\/metadata$/, ''));
     //console.log('portalPacakgeUrl', portalPackageUrl)
 
     // lookup portal mapping to determine portal app settings
@@ -25,7 +27,6 @@ module.exports.processController = (req, res) => {
 
     console.log('processController 2', portalPackageUrl, req.body, portalHost, mapping, registrationKey)
 
-    const form = querystring.parse(req.body);
     const signedUrl = generateSignedUrl(form.redirectUrl, req.body, registrationKey);
     res.set('Location', signedUrl);
     return res.status(307).end();
